20e1781d4bd9aa63e5126eaa212fc06863b8a4f4
User <user@MacBook-Pro-Danil.local>
2023-01-03 19:21:37 +0200

    fixed header

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const hero = document.querySelector('.hero')
+	const header = document.querySelector('.header')
+	const scrollItem = document.querySelectorAll('.scroll-item')
+
+	const headerFixed = () => {
+		let scrollTop = window.scrollY
+		let heroCenter = hero.offsetHeight / 2
+		if (scrollTop >= heroCenter) {
+			header.classList.add('fixed')
+			hero.style.marginTop = `${header.offsetHeight}px`
+		} else {
+			header.classList.remove('fixed')
+			hero.style.marginTop = `0px`
+		}
+	}
+
+	headerFixed( )
+	window.addEventListener('scroll', () => {
+		headerFixed()
+	})
+})
